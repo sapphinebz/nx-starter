cdf7982b3b09284e25f54774f7cabef930b9b459
Thanadit Buthong <thanadit.buthong@accenture.com>
2021-06-28 13:51:58 +0700

    เพิ่ม post get post patch put

--- libs/apps/http/src/lib/http.service.ts
+++ libs/apps/http/src/lib/http.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { EMPTY } from 'rxjs';
@@ -8,22 +8,8 @@ import { catchError } from 'rxjs/operators';
 export class HttpService {
   constructor(public http: HttpClient) {}
 
-  get<R = any>(
-    path: string,
-    condition: { [params: string]: any } = {},
-    getRidNull = false
-  ) {
-    let payload: any;
-    if (getRidNull) {
-      payload = {};
-      Object.entries(condition).forEach(([key, value]) => {
-        if (value !== null) {
-          payload[key] = value;
-        }
-      });
-    } else {
-      payload = { ...condition };
-    }
+  get<R = any>(path: string, condition: { [params: string]: any } = {}) {
+    const payload = condition;
     const params = new HttpParams({ fromObject: payload });
     return this.http
       .get<R>(`${environment.baseUrl}${path}`, { params })
@@ -33,5 +19,60 @@ export class HttpService {
           return EMPTY;
         })
       );
+  }
+
+  post<R = any>(path: string, body: any) {
+    return this.http.post<R>(`${environment.baseUrl}${path}`, body).pipe(
+      catchError((err) => {
+        console.error(err);
+        return EMPTY;
+      })
+    );
+  }
+
+  postFormData<R = any>(path: string, formData: FormData) {
+    return this.http.post<R>(`${environment.baseUrl}${path}`, formData).pipe(
+      catchError((err) => {
+        console.error(err);
+        return EMPTY;
+      })
+    );
+  }
+
+  put<R = any>(path: string, body: any) {
+    return this.http.put<R>(`${environment.baseUrl}${path}`, body);
+  }
+
+  patch<R = any>(path: string, body: any) {
+    return this.http.patch<R>(`${environment.baseUrl}${path}`, body);
+  }
+
+  delete<R = any>(path: string, payload?: any) {
+    if (!payload) {
+      const options = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+        }),
+        body: payload,
+      };
+      return this.http.delete<R>(`${environment.baseUrl}${path}`, options);
+    } else {
+      return this.http.delete<R>(`${environment.baseUrl}${path}`);
+    }
+  }
+
+  /**
+   * สร้าง object เงื่อนไขใหม่ที่ value ของ properties ไม่ null
+   * @param condition เงื่อนไข
+   * @returns object ที่ถูก clone ที่ properties ไม่ null
+   */
+  purifyNotNull(condition: { [params: string]: any }) {
+    const payload: { [params: string]: any } = {};
+    Object.entries(condition).forEach(([key, value]) => {
+      if (value !== null) {
+        payload[key] = value;
+      }
+    });
+    return payload;
   }
 }
